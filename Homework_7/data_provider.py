81007c1ca389a6df2495a48f4147c0b846a54193
OlgaP11 <ooolchaaa@inbox.ru>
2023-01-23 10:53:25 +0300

    Add work with different format in data_provider

--- Homework_7/data_provider.py
+++ Homework_7/data_provider.py
@@ -2,5 +2,8 @@ def get_data(path):
     new_data = list()
     with open (path,'r', encoding = 'utf-8') as data:
         for line in data:
-            new_data.append(line.split(','))
+            if ',' in line:
+                new_data.append(line.split(','))
+            else:
+                new_data.append(line.split())
     return new_data
